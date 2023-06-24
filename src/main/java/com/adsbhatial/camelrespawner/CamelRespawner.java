a78e9419863ca8db124c4998e312ac00c3e624b2
Ashish Deep Singh <ashishdeepsinghbhatial@gmail.com>
2023-06-24 20:38:27 +0530

    Intial working developement plugin

--- src/main/java/com/adsbhatial/camelrespawner/CamelRespawner.java
+++ src/main/java/com/adsbhatial/camelrespawner/CamelRespawner.java
@@ -1,17 +1,171 @@
 package com.adsbhatial.camelrespawner;
 
+import com.adsbhatial.camelrespawner.Listeners.SpawnListeners;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.ChatColor;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import java.nio.file.StandardCopyOption;
+import java.security.CodeSource;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import static org.spigotmc.SpigotConfig.config;
+
+
+@SuppressWarnings("unused")
 public final class CamelRespawner extends JavaPlugin {
+    public final NamespacedKey NAME_KEY = new NamespacedKey(this, "camel");
+    public final static Logger logger = Logger.getLogger("Minecraft");
+    static String THIS_NAME;
+    static String THIS_VERSION;
 
+    public static boolean debug;
+
+    YamlConfiguration oldconfig = new YamlConfiguration();
+    String configVersion = "1.0.0";
+    String pluginName = THIS_NAME;
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        long startTime = System.currentTimeMillis();
+        System.out.println("CamelRespawner Started");
+        loadConfig();
+        getServer().getPluginManager().registerEvents(new SpawnListeners(this), this);
+        consoleInfo(ChatColor.BOLD + "ENABLED" + ChatColor.RESET + " - Loading took " + LoadTime(startTime));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        consoleInfo(ChatColor.BOLD + "DISABLED" + ChatColor.RESET);
+    }
+
+    public void loadConfig(){
+        debug = getConfig().getBoolean("debug", false);
+        oldconfig = new YamlConfiguration();
+        THIS_NAME = this.getDescription().getName();
+        THIS_VERSION = this.getDescription().getVersion();
+        if(!getConfig().getBoolean("console.longpluginname", true)) {
+            pluginName = "CR";
+        }else {
+            pluginName = THIS_NAME;
+        }
+
+        try{
+            if(!this.getDataFolder().exists()){
+                this.getDataFolder().mkdirs();
+            }
+            File file = new File(this.getDataFolder(), "config.yml");
+            //this.getLogger().info("" + file);
+            if(!file.exists()){
+                saveResource("config.yml", true);
+            }
+        }catch(Exception exp){
+            debug = true;
+            exp.printStackTrace();
+        }
+
+        try {
+            oldconfig.load(new File(getDataFolder(), "config.yml"));
+        } catch (IOException | InvalidConfigurationException e1) {
+            e1.printStackTrace();
+        }
+
+        String checkConfigVersion = oldconfig.getString("version", "1.0.0");
+        System.out.println("config version" + checkConfigVersion);
+        if(checkConfigVersion != null) {
+            if (!checkConfigVersion.equalsIgnoreCase(configVersion)) {
+                try {
+                    Path fileFrom = Paths.get(getDataFolder() + "" + File.separatorChar + "config.yml");
+                    Path fileTo = Paths.get(getDataFolder() + "" + File.separatorChar + "old_config.yml");
+                    CopyOption[] options = new CopyOption[]{
+                            StandardCopyOption.REPLACE_EXISTING,
+                            StandardCopyOption.COPY_ATTRIBUTES
+                    };
+                    Files.copy(fileFrom, fileTo,options);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                saveResource("config.yml", true);
+
+                try {
+                    config.load(new File(getDataFolder(), "config.yml"));
+                } catch (IOException | InvalidConfigurationException e1) {
+                    //logWarn("Could not load config.yml");
+                    e1.printStackTrace();
+                }
+                try {
+                    oldconfig.load(new File(getDataFolder(), "old_config.yml"));
+                } catch (IOException | InvalidConfigurationException e1) {
+                    e1.printStackTrace();
+                }
+                config.set("debug", oldconfig.get("debug", false));
+                config.set("version", oldconfig.get("version", "1.0.0"));
+                config.set("cat_to_camel_chance", oldconfig.get("cat_to_camel_chance", 0.75));
+                config.set("camels.check_camels", oldconfig.get("camels.check_camels", true));
+                config.set("camels.max_camels_nearby", oldconfig.get("camels.check_camels", 1));
+                config.set("camels.search_radius", oldconfig.get("camels.search_radius", 20));
+                try {
+                    config.save(new File(getDataFolder(), "config.yml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String LoadTime(long startTime) {
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(elapsedTime);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime) % 60;
+        long milliseconds = elapsedTime % 1000;
+
+        if (minutes > 0) {
+            return String.format("%d min %d s %d ms.", minutes, seconds, milliseconds);
+        } else if (seconds > 0) {
+            return String.format("%d s %d ms.", seconds, milliseconds);
+        } else {
+            return String.format("%d ms.", elapsedTime);
+        }
+    }
+
+
+    public  void log(String logMessage){
+        logger.info(ChatColor.YELLOW + "" + pluginName + ChatColor.RESET + " " + logMessage + ChatColor.RESET);
+    }
+    public  void logDebug(String logMessage){
+        log(" " + THIS_VERSION + ChatColor.RED + ChatColor.BOLD + " [DEBUG] " + ChatColor.RESET + logMessage);
+    }
+    public void logWarn(String logMessage){
+        log(" " + THIS_VERSION + ChatColor.RED + ChatColor.BOLD + " [WARNING] " + ChatColor.RESET + logMessage);
+    }
+    public	void log(Level level, String dalog){
+        logger.log(level, ChatColor.YELLOW + "" + dalog );
+    }
+
+    public void consoleInfo(String state) {
+        logger.info(ChatColor.YELLOW + "**************************************" + ChatColor.RESET);
+        logger.info(ChatColor.GREEN + THIS_NAME + " v" + THIS_VERSION + ChatColor.RESET + " is " + state);
+        logger.info(ChatColor.YELLOW + "**************************************" + ChatColor.RESET);
     }
 }
--- src/main/java/com/adsbhatial/camelrespawner/Listeners/SpawnListeners.java
+++ src/main/java/com/adsbhatial/camelrespawner/Listeners/SpawnListeners.java
@@ -1,4 +1,143 @@
 package com.adsbhatial.camelrespawner.Listeners;
 
-public class SpawnListeners {
+import com.adsbhatial.camelrespawner.CamelRespawner;
+import com.adsbhatial.camelrespawner.util.CamelRespawnerLib;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.World.Environment;
+import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Camel;
+import org.bukkit.entity.Cat;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SpawnListeners implements Listener {
+    CamelRespawner camelRespawner;
+    String THIS_VERSION;
+    String THIS_NAME;
+    String pluginName;
+    boolean debug;
+    public final static Logger logger = Bukkit.getLogger();
+    public SpawnListeners(final CamelRespawner plugin){
+        camelRespawner = plugin;
+        debug          = plugin.debug;
+        THIS_NAME      = plugin.getDescription().getName();
+        THIS_VERSION   = plugin.getDescription().getVersion();
+        if(!plugin.getConfig().getBoolean("console.longpluginname", true)) {
+            pluginName = "CR";
+        }else {
+            pluginName = THIS_NAME;
+        }
+    }
+    @EventHandler
+    public void onCreatureSpawnEvent(CreatureSpawnEvent event){
+        double spawnChance  = (double)  camelRespawner.getConfig().get("cat_to_camel_chance", 0.75);
+        boolean checkCamels = (boolean) camelRespawner.getConfig().get("camels.check_camels", true);
+        int searchRadius    = (int)     camelRespawner.getConfig().get("camels.search_radius", 20);
+        int maxCamels       = (int)     camelRespawner.getConfig().get("camels.max_camels_nearby", 1);;
+
+        Entity entity = event.getEntity();
+        if(entity.getWorld().getEnvironment() != Environment.NORMAL){
+            return;
+        }
+        else{
+            if(entity instanceof Cat){
+                Cat cat = (Cat) entity;
+                Location catLocation = cat.getLocation();
+                Biome catBiome = catLocation.getBlock().getBiome();
+                World catWorld = cat.getWorld();
+                if(catBiome == Biome.DESERT){
+                    if (!cat.isTamed()){
+                        if(debug){
+                            logDebug("SpawnChance : " + spawnChance);
+                            logDebug("searchRadius : " + searchRadius);
+                            logDebug("maxCamels : " + maxCamels);
+                            logDebug("checkCamels : " + checkCamels);
+                            logDebug("A valid natural cat has spawned");
+                        }
+
+                        boolean insideDesertVillage = CamelRespawnerLib.entityInsideDesertVillage(cat,false);
+
+                        if(insideDesertVillage){
+                            if(debug){logDebug("Cat is inside a desert village structure");}
+                            if(!checkCamels || !checkCamelsInRadius(cat,searchRadius,maxCamels)){
+                                if(isCamelSpawnable(spawnChance)){
+                                    event.setCancelled(true);
+                                    catWorld.spawn(catLocation, Camel.class);
+                                    if(debug){ logDebug("Camel has been spawned");}
+                                    Collection<Entity> collection = catWorld.getNearbyEntities(catLocation, 2, 2, 2);
+                                    for (Entity e : collection) {
+                                        if(e instanceof Camel) {
+                                            e.getPersistentDataContainer().set(camelRespawner.NAME_KEY, PersistentDataType.STRING, "CamelRespawner");
+                                        }
+                                    }
+                                }
+                            }
+                            else{
+                                if(debug){ logDebug("There are more camels than max camels within search radius: " + searchRadius);}
+                                return;
+                            }
+                        }
+                    }
+                    else{
+                        if(debug){ logDebug("A unnatural cat is spawned inside a village");}
+                        return;
+                    }
+                }
+                else{
+                    if(debug){ logDebug("Cat spawned in non desert");}
+                    return;
+                }
+            }
+            else{
+                return;
+            }
+        }
+    }
+
+    private boolean checkCamelsInRadius(Entity entity, int radius, int maxCamels){
+        int camelsInRadius = 0;
+        Block spawnBlock = entity.getLocation().getBlock();
+        for(Camel camel : spawnBlock.getWorld().getEntitiesByClass(Camel.class)) {
+            double distance = camel.getLocation().distance(spawnBlock.getLocation());
+            if(distance < radius) {
+                camelsInRadius++;
+                if (camelsInRadius >= maxCamels){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isCamelSpawnable(double chance){
+        double chancepercent = Math.random();
+        if (chance > chancepercent){
+            return true;
+        }
+        return false;
+    }
+
+    public  void log(String logMessage){
+        logger.info(ChatColor.YELLOW + "" + pluginName + ChatColor.RESET + " " + logMessage + ChatColor.RESET);
+    }
+    public  void logDebug(String logMessage){
+        log(" " + THIS_VERSION + ChatColor.RED + ChatColor.BOLD + " [DEBUG] " + ChatColor.RESET + logMessage);
+    }
+    public void logWarn(String logMessage){
+        log(" " + THIS_VERSION + ChatColor.RED + ChatColor.BOLD + " [WARNING] " + ChatColor.RESET + logMessage);
+    }
+    public	void log(Level level, String logMessage){
+        logger.log(level, ChatColor.YELLOW + "" + logMessage );
+    }
 }
